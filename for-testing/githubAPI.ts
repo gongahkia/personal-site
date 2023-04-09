212db3adec1b4b0e4ff2e823d055800e4d5c6db6
gongahkia <angryapplegravy@gmail.com>
2023-04-09 23:18:09 +0800

    oki

--- for-testing/githubAPI.ts
+++ for-testing/githubAPI.ts
@@ -1,23 +1,37 @@
-// figure out how to use API calls 
+// sort each repo by date, choose 4 with the dates that are the earliest
 
 async function contactAPI(targetUrl:string) {
     const response = await fetch(targetUrl);
     const data = await response.json();
-    
-    for (var repo of data) {
-        let repoName:string = repo.name;
-        let repoUrl:string = repo.svn_url;
-        let repoLastCommitPushedDate:string = repo.pushed_at.split("T")[0];
-        let repoLastCommitPushedTime:string = repo.pushed_at.split("T")[1].split("Z")[0];
-        let repoDescription:string = repo.description;
-        let repoLanguages:string = repo.language;
-        console.log(`${repoName}\n${repoUrl}\n${repoLastCommitPushedDate}\n${repoLastCommitPushedTime}\n${repoDescription}\n${repoLanguages}`);
-        console.log("------");
+
+    // consolidate important information and sort latest github repo by date
+    const infoArray:any[] = [];
+    interface RepoInfo {
+        repoName:string;
+        repoUrl:string;
+        repoLastCommitPushedDate:string;
+        repoLastCommitPushedTime:string;
+        repoDescription:string;
+        repoLanguages:string
     }
 
+    for (var repo of data) {
+        let givenRepo:RepoInfo = {
+            repoName: repo.name,
+            repoUrl: repo.svn_url,
+            repoLastCommitPushedDate: repo.pushed_at.split("T")[0],
+            repoLastCommitPushedTime: repo.pushed_at.split("T")[1].split("Z")[0],
+            repoDescription: repo.description,
+            repoLanguages: repo.language
+        }
+
+        infoArray.push(givenRepo);
+        //console.log(`${repoName}\n${repoUrl}\n${repoLastCommitPushedDate}\n${repoLastCommitPushedTime}\n${repoDescription}\n${repoLanguages}`);
+        //console.log(givenRepo.repoName);
+        //console.log("------");
+    }
+    console.log(infoArray);
     console.log(`${data.length} public repos total`);
 }
 
 contactAPI("https://api.github.com/users/gongahkia/repos");
-
-
