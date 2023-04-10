c84e5cdbfa5c5e1ad344b4522632d2ca6198f3d3
gongahkia <angryapplegravy@gmail.com>
2023-04-10 11:21:07 +0800

    figured out logic

--- for-testing/githubAPI.ts
+++ for-testing/githubAPI.ts
@@ -1,10 +1,12 @@
-// sort each repo by date, choose 4 with the dates that are the earliest
+// sort each repo by date, choose 3 with the dates that are the earliest
 
 async function contactAPI(targetUrl:string) {
     const response = await fetch(targetUrl);
     const data = await response.json();
 
     // consolidate important information and sort latest github repo by date
+    const permDateArray:number[][] = [];
+    const top3DateArray:number[][] = [];
     const infoArray:any[] = [];
     interface RepoInfo {
         repoName:string;
@@ -24,14 +26,39 @@ async function contactAPI(targetUrl:string) {
             repoDescription: repo.description,
             repoLanguages: repo.language
         }
-
+        
         infoArray.push(givenRepo);
-        //console.log(`${repoName}\n${repoUrl}\n${repoLastCommitPushedDate}\n${repoLastCommitPushedTime}\n${repoDescription}\n${repoLanguages}`);
-        //console.log(givenRepo.repoName);
-        //console.log("------");
+
+        const dateYear:number = +givenRepo.repoLastCommitPushedDate.split("-")[0];
+        const dateMonth:number = +givenRepo.repoLastCommitPushedDate.split("-")[1];
+        const dateDay:number = +givenRepo.repoLastCommitPushedDate.split("-")[2];
+
+        const tempDateArray:number[] = [];
+        tempDateArray.push(dateYear, dateMonth, dateDay);
+        permDateArray.push(tempDateArray);
     }
+    console.log(permDateArray);
     console.log(infoArray);
     console.log(`${data.length} public repos total`);
+
+    const currentDate:string = new Date().toLocaleDateString();
+    const currentYear:number = +currentDate.split("/")[2];
+    const currentMonth:number = +currentDate.split("/")[0];
+    const currentDay:number = +currentDate.split("/")[1];
+    console.log(currentYear, currentMonth, currentDay);
+
+    for (let date of permDateArray) {
+        if (date[0] == currentYear && date[1] == currentMonth) {
+            top3DateArray.push(infoArray[permDateArray.indexOf(date)]);
+        }
+    }
+    
+    // to remove an item from final Array to only display 3 values max
+    if (top3DateArray.length > 3) {
+        delete top3DateArray[Math.floor(Math.random() * 3)];
+    }
+
+    console.log(top3DateArray);
 }
 
 contactAPI("https://api.github.com/users/gongahkia/repos");
